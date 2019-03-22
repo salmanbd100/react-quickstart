21038c330ddd23989d62c6701417dfd7fb1013b1
Salman Rahman <salman@Salmans-MacBook-Pro.local>
2019-03-23 01:24:06 +0600

    clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
-import "./App.css";
-import Count from './components/Count'
-import Control from './components/Control'
+import Count from "./components/Count";
+import Control from "./components/Control";
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <h1>Redux Learning</h1>
-          <Count />
-          <Control />
+          <div className="container">
+            <h1>Redux Learning</h1>
+            <Count />
+            <Control />
+          </div>
         </div>
       </Provider>
     );
