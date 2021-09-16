50008d85a0cc167bd8abbf64e5a802287a8f6e79
salman <salman.rahman@selise.ch>
2021-09-16 20:47:11 +0600

    react prettier setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  console.log('obj');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +11,15 @@ function App() {
         >
           Learn React
         </a>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.Cum quae aliquam molestias eos obcaecati! Ut aperiam sequi
+        sed est molestias, eveniet ratione distinctio atque esse quo.
+        Cumque rem laboriosam optio? Praesentium, eligendi! Accusamus
+        animi fugiat harum ea minus, ex reprehenderit ullam ad assumenda
+        autem, qui neque perferendis praesentium error, laborum nemo voluptates!
+        Nulla at sit pariatur assumenda voluptatibus labore exercitationem expedita
+        consequatur. Aliquid perferendis nesciunt perspiciatis beatae deserunt a ab
+        nobis labore nostrum non quae ratione consequatur voluptatibus ad fugit molestiae
+        debitis cum nemo, doloribus sit numquam, vel incidunt ducimus! Autem aliquam,
       </header>
     </div>
   );
